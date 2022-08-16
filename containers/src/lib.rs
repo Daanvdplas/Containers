ef755c1cb3c234ce03b3f0b4a151691d2f7e3f36
Daan van der Plas <daanvanderplas@MacBook-Pro-28.local>
2022-08-16 17:16:24 +0200

    start project

--- containers/src/lib.rs
+++ containers/src/lib.rs
@@ -1,8 +1,39 @@
+use std::ptr::NonNull;
+
+struct MyVec<T> {
+    ptr: NonNull<T>,
+    len: usize,
+    capacity: usize,
+}
+
+impl<T> MyVec<T> {
+    fn new() -> Self {
+        Self {
+            ptr: NonNull::<T>::dangling(), 
+            len: 0,
+            capacity: 0,
+        }
+    }
+
+    fn capacity(&self) -> usize {
+        self.capacity
+    }
+    
+    fn len(&self) -> usize {
+        self.len
+    }
+
+    push(&self) -> 
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_myvec() {
+        let mut vec: MyVec<usize> = MyVec::new();
+        assert_eq!(vec.len(), 0);
+        assert_eq!(vec.capacity(), 0);
     }
 }
---
+++ containers/target/debug/deps/containers-666ec95ad363a9d8.d
@@ -0,0 +1,5 @@
+/Users/daanvanderplas/Documents/Github/containers/containers/target/debug/deps/containers-666ec95ad363a9d8: src/lib.rs
+
+/Users/daanvanderplas/Documents/Github/containers/containers/target/debug/deps/containers-666ec95ad363a9d8.d: src/lib.rs
+
+src/lib.rs:
