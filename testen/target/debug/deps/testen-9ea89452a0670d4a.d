4641a68db225044e19e7e0cc538877ae6c37a30f
Daan Van der plas <dvan-der@f1r1s11.codam.nl>
2022-08-23 17:22:20 +0200

    edit gitignore

---
+++ testen/target/debug/deps/testen-9ea89452a0670d4a.d
@@ -0,0 +1,5 @@
+/Users/dvan-der/Documents/CODAM/Containers/testen/target/debug/deps/testen-9ea89452a0670d4a: src/main.rs
+
+/Users/dvan-der/Documents/CODAM/Containers/testen/target/debug/deps/testen-9ea89452a0670d4a.d: src/main.rs
+
+src/main.rs:
---
+++ testen/target/debug/testen.d
@@ -0,0 +1,1 @@
+/Users/dvan-der/Documents/CODAM/Containers/testen/target/debug/testen: /Users/dvan-der/Documents/CODAM/Containers/testen/src/main.rs
